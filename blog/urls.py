926bb50bbf2b966eb2c20f1777eeba5a5f238eb2
islamovdanil <mr.ozmon@gmail.com>
2018-09-04 13:30:59 +0300

    Прикручены посты, есть вьюхи. Проблемы с bootstrap и ссылками

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+from django.conf.urls import *
+from blog.views import PostsListView, PostDetailView
+
+urlpatterns = [
+    url(r'^$', PostsListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view()),
+]
--- zapiskiit/urls.py
+++ zapiskiit/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = ['',
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
