2f80d49d1cc7fa15997e1745558b06465f7b14be
islamovdanil <mr.ozmon@gmail.com>
2018-09-04 15:40:13 +0300

    Воюю с bootstrap

--- zapiskiit/settings.py
+++ zapiskiit/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'zapiskiit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'static', 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
